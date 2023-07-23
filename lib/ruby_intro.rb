36aa207ec0ab0f1d8e1be1e8df0724cc2f788f0f
Makkrapongg <110651269+Makkrapongg@users.noreply.github.com>
2023-07-23 22:17:28 +0700

    First

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,51 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.length == 0
+  return arr[0]if arr.length == 1
+  return sum arr.sort {|i,j| j<=>i}.take(2)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.lenght == 0 || arr.lenght == 1
+  for i in 0...arr.lenght-1
+    for j in i+1..arr.lenght
+      return true if arr[i]+arr[j] == n
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !(s =~ /^(?![aeiou])[a-z]/i).nil?
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  !(s =~ /^(?:[01]*00|0)$/).nil?
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize (isbn,price)
+    @isbn = isbn
+    @price = price
+    raise ArgumentError if isbn == '' || price <= 0
+  end
+
+  def price_as_string
+    return "$%0.02f" %@price
+  end
 end
